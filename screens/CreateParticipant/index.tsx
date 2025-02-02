a4d326514471237d5fdedd00b1e58cbbb9aa29fe
frandev <falconherediafrancodev@gmail.com>
2025-02-02 01:11:16 -0300

    fix bug repeat avatar

--- screens/CreateParticipant/index.tsx
+++ screens/CreateParticipant/index.tsx
@@ -4,16 +4,29 @@ import { HeaderCreateParticipant } from "./components";
 import { CustomButton } from "@/components/ui";
 import { ScrollView, View } from "react-native";
 import { styles } from "./styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getRandomAvatarAndColor } from "@/utils/randomUtils";
 import { useNavigation } from "@react-navigation/native";
 import { useBillContext } from "@/context";
 export const CreateParticipant = () => {
-  const { addParticipant } = useBillContext();
+  const { addParticipant, bills } = useBillContext();
   const navigation = useNavigation();
+  const billId = "124-s";
   const [display, setDisplay] = useState<string>("");
   const [avatar, setAvatar] = useState(getRandomAvatarAndColor());
   const [name, setName] = useState("");
+  const currentBill = bills.find((bill) => bill.id === billId);
+  const usedAvatars = currentBill?.participants.map(
+    (participant) => participant.avatar,
+  );
+
+  useEffect(() => {
+    let randomAvatar = getRandomAvatarAndColor();
+    while (usedAvatars?.includes(randomAvatar.avatar)) {
+      randomAvatar = getRandomAvatarAndColor();
+    }
+    setAvatar(randomAvatar);
+  }, [usedAvatars]);
 
   const handleSave = () => {
     const payment = parseFloat(display) || 0;
